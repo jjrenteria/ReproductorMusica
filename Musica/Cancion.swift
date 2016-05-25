9b9b38a7c5c9d16918c89941c2d469cf51a547a3
Juan Jose Renteria <juanjoserenteria@macbook-pro-de-juan.local>
2016-05-24 19:03:48 -0500

    Version inicial

---
+++ Musica/Cancion.swift
@@ -0,0 +1,17 @@
+
+//
+//  Cancion.swift
+//  Musica
+//
+//  Created by Juan Jose Renteria on 23/05/16.
+//  Copyright © 2016 Juan Jose Renteria. All rights reserved.
+//
+
+import Foundation
+
+struct Cancion {
+    var path: String
+    var nombreFoto: String
+    var titulo: String
+    
+}
--- Musica/ViewController.swift
+++ Musica/ViewController.swift
@@ -7,19 +7,95 @@
 //
 
 import UIKit
+import AVFoundation
+import CoreMotion
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var tituloCancion: UILabel!
+    
+    @IBOutlet weak var volumenControl: UIStepper!
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    @IBOutlet weak var fotoCancion: UIImageView!
+    var arregloCanciones: [Cancion] = []
+    var reproductor : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tituloCancion.text = "  "
+        arregloCanciones.append( Cancion(path: "LINKINPARK", nombreFoto: "LINKINPARK.png", titulo: "What ive done") )
+        arregloCanciones.append( Cancion(path: "RickSpringfieldHQ", nombreFoto: "RickSpringfieldHQ.jpg", titulo: "Jessie's Girl") )
+        
+        arregloCanciones.append( Cancion(path: "BEATLES", nombreFoto: "BEATLES.jpg", titulo: "Beatles") )
+        arregloCanciones.append( Cancion(path: "ELVIS", nombreFoto: "ELVIS.jpg", titulo: "All Shook") )
+        arregloCanciones.append( Cancion(path: "aerosmith-cryin", nombreFoto: "aerosmith-cryin.jpeg", titulo: "Cryin") )
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Mark UIPickerViewDataSource
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
     }
-
-
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return arregloCanciones.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return arregloCanciones[ row ].titulo
+    }
+    
+    // Mark UIPickerViewDelegate
+    
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let sonidoUrl = NSBundle.mainBundle().URLForResource( arregloCanciones[ row ].path, withExtension: "mp3")
+        do {
+            try reproductor = AVAudioPlayer(contentsOfURL: sonidoUrl!)
+            tituloCancion.text = arregloCanciones[row].titulo
+            fotoCancion.image = UIImage(named: arregloCanciones[row].nombreFoto)
+            
+        } catch {
+            print( "Error al reproducir el sonido" )
+        }
+    
+    }
+  
+    
+    @IBAction func play(sender: UIButton) {
+        if !reproductor.playing {
+            reproductor.play()
+        }
+    }
+    
+    @IBAction func pause(sender: UIButton) {
+        if reproductor.playing {
+            reproductor.pause()
+        }
+    }
+    
+    @IBAction func stop(sender: UIButton) {
+        if reproductor.playing {
+            reproductor.stop()
+            reproductor.currentTime = 0
+        }
+    }
+    
+    @IBAction func randomPlay(sender: UIButton) {
+        
+        let row = Int( arc4random_uniform( UInt32(arregloCanciones.count )) )
+        picker.selectRow(row, inComponent: 0, animated: true)
+        pickerView(self.picker, didSelectRow: row, inComponent: 0 )
+    }
+  
+    // El valor de volumen entre 0 y 1
+    @IBAction func volumen(sender: UIStepper) {
+        reproductor.volume = Float( sender.value / 100 )
+    }
+    
+    
 }
 
